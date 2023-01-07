f0d8415f7d407135f0bd214eb7df1d47eced8c33
Mipmipp <106466126+Mipmipp@users.noreply.github.com>
2023-01-07 20:10:28 -0300

    Create a function to create a card for every character
    Also give a click event for every card to show their modal when user clicks them

--- src/ui.js
+++ src/ui.js
@@ -19,3 +19,35 @@ export function showCharacter(character) {
     $location.innerText = `Location: ${character.location.name}`;
 }
 
+export function showCharacters(characters) {
+    const $charactersList = document.querySelector('#characters-list');
+    const $modal = document.querySelector('#modal');
+    const $buttonClose = document.querySelector('#btn-close');
+    $charactersList.innerHTML = '';
+
+    characters.forEach((character) => {
+        const $column = document.createElement('div');
+        const $card = document.createElement('div');
+        const $cardBody = document.createElement('div');
+        const $name = document.createElement('p');
+        const $image = document.createElement('img');
+
+        $column.className = 'col-6 col-xl-3';
+        $card.className = 'card';
+        $card.title = character.name;
+        $card.addEventListener('click', () => getCharacter(showCharacter, character.id));
+        $card.addEventListener('click', () => $modal.showModal());
+        $buttonClose.addEventListener('click', () => $modal.close());
+        $cardBody.className = 'card-body';
+        $image.src = character.image;
+        $image.alt = `${character.name} image`;
+        $name.className = 'card-text';
+        $name.textContent = character.name;
+
+        $card.appendChild($image);
+        $card.appendChild($cardBody);
+        $cardBody.appendChild($name);
+        $column.appendChild($card);
+        $charactersList.appendChild($column);
+    });
+}
