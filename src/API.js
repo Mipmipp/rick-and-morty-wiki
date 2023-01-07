eaf77ff6ee039d92ae3e76da97e9efde8bcef767
Mipmipp <106466126+Mipmipp@users.noreply.github.com>
2023-01-07 20:03:46 -0300

    Create a function to get characters from the API and use that response as parameter of a callback function

--- src/API.js
+++ src/API.js
@@ -1,2 +1,9 @@
 const URL = 'https://rickandmortyapi.com/api/character';
 
+export function getCharacters(callback, page = URL) {
+    fetch(page)
+    .then((response) => response.json())
+    .then((data) => callback(data.results))
+    .catch((error) => console.log(error));
+}
+
