55c425b6f5e5ef11df2b25df5f81472789ec107f
Mipmipp <106466126+Mipmipp@users.noreply.github.com>
2023-01-07 20:12:41 -0300

    Create a function to initialize the page

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 import { getCharacters, getTotalPages } from './API.js';
 import { makePagination, showCharacters } from './ui.js';
 
+ function initialize() {
+    getTotalPages(makePagination);
+    getCharacters(showCharacters);
+}
+
