c8d489abe9977b0f0aa430fb64eeed396a0fefc1
Mipmipp <106466126+Mipmipp@users.noreply.github.com>
2023-05-29 22:07:52 -0300

    Create mocks files

---
+++ src/__mocks__/listCharactersMock.js
@@ -0,0 +1,50 @@
+export const listCharactersMock = [
+    {
+        id: 1,
+        name: "Rick Sanchez",
+        status: "Alive",
+        species: "Human",
+        type: "",
+        gender: "Male",
+        origin: {
+            name: "Earth (C-137)",
+            url: "https://rickandmortyapi.com/api/location/1",
+        },
+        location: {
+            name: "Citadel of Ricks",
+            url: "https://rickandmortyapi.com/api/location/3",
+        },
+        image: "https://rickandmortyapi.com/api/character/avatar/1.jpeg",
+        episodes: [
+            "https://rickandmortyapi.com/api/episode/1",
+            "https://rickandmortyapi.com/api/episode/2",
+            "https://rickandmortyapi.com/api/episode/3",
+        ],
+        url: "https://rickandmortyapi.com/api/character/1",
+        created: "2017-11-04T18:48:46.250Z",
+    },
+    {
+        id: 2,
+        name: "Morty Smith",
+        status: "Alive",
+        species: "Human",
+        type: "",
+        gender: "Male",
+        origin: {
+            name: "unknown",
+            url: "",
+        },
+        location: {
+            name: "Citadel of Ricks",
+            url: "https://rickandmortyapi.com/api/location/3",
+        },
+        image: "https://rickandmortyapi.com/api/character/avatar/2.jpeg",
+        episodes: [
+            "https://rickandmortyapi.com/api/episode/1",
+            "https://rickandmortyapi.com/api/episode/2",
+            "https://rickandmortyapi.com/api/episode/3",
+        ],
+        url: "https://rickandmortyapi.com/api/character/2",
+        created: "2017-11-04T18:50:21.651Z",
+    },
+];
