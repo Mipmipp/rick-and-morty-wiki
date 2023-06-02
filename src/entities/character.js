3f87bb70bf44c7e763c3801bdbb3657884946520
Mipmipp <106466126+Mipmipp@users.noreply.github.com>
2023-06-01 22:05:46 -0300

    create character entity

---
+++ src/entities/character.js
@@ -0,0 +1,12 @@
+export default class Character {
+    constructor(id, name, status, specie, gender, origin, location, image) {
+        this.id = id;
+        this.name = name;
+        this.status = status;
+        this.specie = specie;
+        this.gender = gender;
+        this.origin = origin;
+        this.location = location;
+        this.image = image;
+    }
+}
